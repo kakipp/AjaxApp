d5b405eef95f584be96384e25fd9a8be3f560a15
kakipp <75791004+kakipp@users.noreply.github.com>
2021-02-11 14:27:40 +0900

    既読機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,7 @@ class PostsController < ApplicationController
             post.update(checked: true)
         end
 
-        item = Post.find(paramss[:id])
+        item = Post.find(params[:id])
         render json: { post: item }
     end
 end
